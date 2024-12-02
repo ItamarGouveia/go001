ca65b4c2199cfe875aea4b0a15444b51a460bf0d
ItamarGouveia <contato.itamargouveia@gmail.com>
2024-12-02 10:43:44 -0300

    03

--- api/html.go
+++ api/html.go
@@ -1,21 +1,12 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func HtmlRendering(w http.ResponseWriter, r *http.Request) {
-	htmlContent := `
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<title>Go HTML Rendering</title>
-	</head>
-	<body>
-		<h1>Hello, this is a HTML document rendered with Go!</h1>
-	</body>
-	</html>
-	`
-	fmt.Fprint(w, htmlContent)
+	htmlFile := "../public/blog/teste.html"
+
+	// Serve o arquivo HTML externo
+	http.ServeFile(w, r, htmlFile)
 }
