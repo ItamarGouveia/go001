de91c67e005beb88a18670f2297d0eff6dad931d
ItamarGouveia <contato.itamargouveia@gmail.com>
2024-12-02 10:49:58 -0300

    03

--- api/html.go
+++ api/html.go
@@ -7,7 +7,7 @@ import (
 
 func HtmlRendering(w http.ResponseWriter, r *http.Request) {
 	// Caminho para o arquivo HTML dentro da pasta "public"
-	htmlFile := filepath.Join("app", "public", "teste.html")
+	htmlFile := filepath.Join("go001", "public", "teste.html")
 
 	// Serve o arquivo HTML da pasta public
 	http.ServeFile(w, r, htmlFile)
