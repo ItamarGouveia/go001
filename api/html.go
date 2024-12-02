daec01504a77b1aa809cdf8ac64e3935587e6873
ItamarGouveia <contato.itamargouveia@gmail.com>
2024-12-02 10:55:10 -0300

    03

--- api/html.go
+++ api/html.go
@@ -1,14 +1,26 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 	"path/filepath"
 )
 
 func HtmlRendering(w http.ResponseWriter, r *http.Request) {
-	// Caminho para o arquivo HTML dentro da pasta "public"
-	htmlFile := filepath.Join("go001", "public", "teste.html")
+	// Caminho para o arquivo HTML na pasta "public"
+	htmlFile := filepath.Join("go001", "public", "seu_arquivo.html")
 
-	// Serve o arquivo HTML da pasta public
-	http.ServeFile(w, r, htmlFile)
+	// Log para verificar o caminho do arquivo
+	log.Printf("Tentando servir o arquivo: %s\n", htmlFile)
+
+	// Verificar se o arquivo existe
+	_, err := filepath.Glob(htmlFile)
+	if err != nil {
+		log.Printf("Erro ao encontrar o arquivo: %v", err)
+		http.Error(w, "Erro interno do servidor", http.StatusInternalServerError)
+		return
+	}
+
+	// Log para confirmar que o arquivo foi servido corretamente
+	log.Printf("Arquivo %s servido com sucesso.\n", htmlFile)
 }
